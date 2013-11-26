17bb4c6e148b8581282dc09596a1352ba179b7c0
Sarah Kim <jiyunie13@gmail.com>
2013-11-25 22:22:25 -0800

    Finished Index/About page

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,52 @@
 $(document).ready(function(){
 
-/*	$('.header-link').hover(function() {
-		$(this).css({'color': '#A6D785'});
-	}, function() {
-		$(this).css({'color': 'white'});
-	});
-*/
-    $('.linkheader').hover(function() {
-        $(this).css({'color': '#7fa958'});
-    }, function() {
-        $(this).css({'color': '#629632'});
-    });
-
-    $('.link').hover(function() {
-        $(this).css({'color': 'grey'});
-    }, function() {
-        $(this).css({'color': 'black'});
-    });
-
-    $('.event-name').hover(function() {
-        $(this).css({'color': 'grey'});
-    }, function() {
-        $(this).css({'color': 'black'});
-    });
-
-    $('.credit').hover(function() {
-        $(this).css({'color': '#7fa958'});
-    }, function() {
-        $(this).css({'color': '#629632'});
-    });
-
-    $('.creditgrey').hover(function() {
-        $(this).css({'color': 'black'});
-    }, function() {
-        $(this).css({'color': 'grey'});
-    });
-
-    $('.tab').click(function() {
-        $('.tab').css({'color': 'grey'});
-        $(this).css({'color': 'white'});
-    });
-
-    $('#panels a').click(function() {
-        var panel_id = $(this).attr('href');
-        $('.panel').hide();
-        $(panel_id).show();
-    });
-
-    $('.photoone').hover(function() {
-        $(this).next().css({'z-index': '3'});
-        $(this).next().fadeIn(400);
+    /** These are the sidebar animations. */
+    
+    $('#sidebar').hover(function() {
+        $('#sidebarfull').animate({"left": "0px"}, 400);
     }, function() {
     });
-
-    $('.phototwo').hover(function() {
+    
+    $('#sidebarfull').hover(function() {
     }, function() {
-        $(this).stop().fadeOut(400);
+        $(this).animate({"left": "-65px"}, 400);
+    });
+
+    $('#abouticon').hover(function() {
+        $('#abouttag').show();
+    }, function() {
+        $('#abouttag').hide();
+    });
+
+    $('#workicon').hover(function() {
+        $('#worktag').show();
+    }, function() {
+        $('#worktag').hide();
+    });
+
+    $('#projectsicon').hover(function() {
+        $('#projectstag').show();
+    }, function() {
+        $('#projectstag').hide();
+    });
+
+    $('#resumeicon').hover(function() {
+        $('#resumetag').show();
+    }, function() {
+        $('#resumetag').hide();
+    });
+
+    $('#emailicon').hover(function() {
+        $('#emailtag').show();
+    }, function() {
+        $('#emailtag').hide();
+    });
+
+    /** These are the other animations on the Index page. */
+    $('.button').hover(function() {
+        $(this).css({'background': '#DDD'});
+    }, function() {
+        $(this).css({'background': '#D0D0D0'});
     });
 
 });
