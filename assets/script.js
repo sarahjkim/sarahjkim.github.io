ad3cdad093c1deca48ad99c3700e1f66fbf4d5b6
Sarah Kim <jiyunie13@gmail.com>
2013-12-03 12:56:25 -0800

    Finished blog page

--- assets/script.js
+++ assets/script.js
@@ -48,11 +48,48 @@ $(document).ready(function(){
         $('#emailtag').hide();
     });
 
-    /** These are the other animations on the Index page. */
+    /** These are the animations on the Index page. */
+    
     $('.button').hover(function() {
         $(this).css({'background': '#DDD'});
     }, function() {
         $(this).css({'background': '#D0D0D0'});
+    });
+
+    /** These are the animations on the Blog page. */
+
+    $('.barpoint').hover(function() {
+        var color = $(this).css('color');
+        if (color == 'rgb(153, 153, 153)') {
+            $(this).css({'color': 'black'});
+        }
+    }, function() {
+        var color = $(this).css('color');
+        if (color == 'rgb(0, 0, 0)') {
+            $(this).css({'color': '#999'});
+        }
+    });
+
+    $('.barpoint').click(function() {
+        $(this).parent().children().css('color', '#999');
+        $(this).css('color', '#00B2EE');
+        var id = $(this).attr('id');
+        $('.blogpost').hide();
+        if (id == 'alltime' | id=='all') {
+            $('.blogpost').show(400);
+        } else if (id == '201311') {
+            $('.201311').show(400);
+        } else if (id == '201312') {
+            $('.201312').show(400);
+        } else if (id == 'christianity') {
+            $('.christianity').show(400);
+        } else if (id == 'cs') {
+            $('.cs').show(400);
+        } else if (id == 'gov') {
+            $('.gov').show(400);
+        } else if (id == 'personal') {
+            $('.personal').show(400);
+        }
     });
 
 });
