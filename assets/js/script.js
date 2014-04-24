e79593c389bbb2275adc34b1733eab764a304ceb
Sarah Kim <jiyunie13@gmail.com>
2014-04-24 00:56:51 -0700

    Complete Website Overhaul
    1. Changed sidebar
    2. Reorganized /assets and /blog
    3. Make Organizations page
    4. Updated Resume & Information

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,51 +3,17 @@ $(document).ready(function(){
     /** These are the sidebar animations. */
     
     $('#sidebar').hover(function() {
-        $('#sidebarfull').animate({"left": "0px"}, 400);
+        $(this).animate({'left': '0px'}, 400);
     }, function() {
+        $(this).animate({'left': '-140px'}, 400);
     });
     
-    $('#sidebarfull').hover(function() {
+    $('.sidebar-item').hover(function() {
+        $(this).css({'color': 'white'});
     }, function() {
-        $(this).animate({"left": "-65px"}, 400);
+        $(this).css({'color': '#CCC'});
     });
-
-    $('#abouticon').hover(function() {
-        $('#abouttag').show();
-    }, function() {
-        $('#abouttag').hide();
-    });
-
-    $('#workicon').hover(function() {
-        $('#worktag').show();
-    }, function() {
-        $('#worktag').hide();
-    });
-
-    $('#projectsicon').hover(function() {
-        $('#projectstag').show();
-    }, function() {
-        $('#projectstag').hide();
-    });
-
-    $('#blogicon').hover(function() {
-        $('#blogtag').show();
-    }, function() {
-        $('#blogtag').hide();
-    });
-
-    $('#resumeicon').hover(function() {
-        $('#resumetag').show();
-    }, function() {
-        $('#resumetag').hide();
-    });
-
-    $('#emailicon').hover(function() {
-        $('#emailtag').show();
-    }, function() {
-        $('#emailtag').hide();
-    });
-
+    
     /** These are the animations on the Index page. */
     
     $('.button').hover(function() {
