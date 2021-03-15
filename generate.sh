80d7709f094c6cf6ee58147d319a09d39dc1af5b
Sarah Kim <jiyunie13@gmail.com>
2021-03-14 19:04:15 -0700

    Add simple bash script to auto-generate html files

---
+++ generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Generates an HTML file given the page name ($1).
+generate_main_html() {
+  chevron -d templates/$1.yaml -p templates/ templates/base.mustache > $1.html
+}
+
+# Generates a blogpost HTML file given the blog ID ($1).
+generate_blogpost_html() {
+  chevron -d blog/templates/id_$1.yaml -p blog/templates/ templates/base.mustache > blog/$1.html
+}
+
+generate_main_html blog
+generate_main_html cs61b
+generate_main_html index
+generate_main_html organizations
+generate_main_html projects
+generate_main_html work
+
+generate_blogpost_html "20131125"
+generate_blogpost_html "20131216"
+generate_blogpost_html "20131227"
+
+exit 0
